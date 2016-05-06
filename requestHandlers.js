8d81c6da68423a552bd183174f6bef24da48dfc6
colon <colon.song@gmail.com>
2016-05-06 17:12:00 +0800

    VSCODE IDE launch.json

--- requestHandlers.js
+++ requestHandlers.js
@@ -27,7 +27,7 @@ function show(response, postData)
     fs.readFile("./tmp/test.png", "binary", function(error, file){
        if(error){
            response.writeHead(500, {"Content-Type":"text/plain"});
-           response.write(error + "\n");
+
            response.end();
        } else {
            response.writeHead(200, {"Content-Type" : "image/png"});
@@ -36,6 +36,9 @@ function show(response, postData)
        }
     });
 }
+
+
+
 
 function upload(response, postData)
 {
