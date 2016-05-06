5b23ae6afcad2b501cce12115c77b5426061b76d
colon <colon.song@gmail.com>
2016-05-06 14:35:24 +0800

    receive post chunk data

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ function startServer(route, handle)
         console.log("DO REQUEST");
         var urlPath = url.parse(request.url).pathname;
         console.log("url path"  + urlPath);
+        
+        // 開始接受請求資料
+        var postData  = "";
+        request.addListener("data", function(postDataChunk){
+            postData += postDataChunk;
+            console.log("get post chunk data: " + postDataChunk );
+        });
+        
+        request.addListener("end", function(){
+           route(handle, pathname, response, postData); 
+        });
+        // 接收資料END
         route(handle, urlPath, response);
     }).listen(8888);
     console.log("Server Start");
